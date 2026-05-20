6a940dfc2394abdca2bdfaebee06cd216dcf6272
deonvdberg1 <deonvdberg1@gmail.com>
2026-05-20 12:02:37 +0200

    Pallet Town final layout (20 May)

--- src/new_game.c
+++ src/new_game.c
@@ -135,9 +135,9 @@ static void ClearFrontierRecord(void)
 static void WarpToTruck(void)
 {
     if (IS_FRLG)
-        SetWarpDestination(MAP_GROUP(MAP_PALLET_TOWN_KHANTO), MAP_NUM(MAP_PALLET_TOWN_KHANTO), WARP_ID_NONE, 6, 6);
+        SetWarpDestination(MAP_GROUP(MAP_PALLET_TOWN_PLAYERS_HOUSE_2F), MAP_NUM(MAP_PALLET_TOWN_PLAYERS_HOUSE_2F), WARP_ID_NONE, 6, 6);
     else
-        SetWarpDestination(MAP_GROUP(MAP_PALLET_TOWN_KHANTO), MAP_NUM(MAP_PALLET_TOWN_KHANTO), WARP_ID_NONE, 5, 7);
+        SetWarpDestination(MAP_GROUP(MAP_INSIDE_OF_TRUCK), MAP_NUM(MAP_INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
     WarpIntoMap();
 }
 
