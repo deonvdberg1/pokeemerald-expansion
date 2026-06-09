fa6c263c1a02de340729a477e8e34b8c7b7397c2
deonvdberg1 <deonvdberg1@gmail.com>
2026-06-09 19:51:05 +0200

    Pewter Gym: 4 new Rock-type trainers before Brock

--- include/constants/opponents_frlg.h
+++ include/constants/opponents_frlg.h
@@ -627,12 +627,17 @@
 #define TRAINER_CHAMPION_REMATCH_BULBASAUR         621
 #define TRAINER_CHAMPION_REMATCH_CHARMANDER        622
 #define TRAINER_CUE_BALL_PAXTON                    623
+#define TRAINER_HIKER_NORTH                       624
+#define TRAINER_YOUNGSTER_SAM                     626
+#define TRAINER_HIKER_TROY                        627
+
+
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is
 //       only space for 25 additional trainers before trainer flag space overflows.
 //       MAX_TRAINERS_COUNT_FRLG can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT_FRLG                      624
+#define TRAINERS_COUNT_FRLG                      628
 #define MAX_TRAINERS_COUNT_FRLG                  768
 
 #endif  // GUARD_CONSTANTS_OPPONENTS_FRLG_H
